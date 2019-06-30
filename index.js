5afc3279b511abdc049539afe0e999fc8980cf4b
Felix Müllner <felix.muellner.96@gmail.com>
2019-06-30 16:58:29 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,68 @@
+require('dotenv').config();
+
+const level = require('level');
+const Parser = require('rss-parser');
+
+const childProcess = require('child_process');
+
+const RSS_FEED = `https://www.youtube.com/feeds/videos.xml?channel_id=${process.env.YOUTUBE_CHANNEL_ID}`;
+
+const db = level('database');
+const parser = new Parser();
+
+checkYouTubeChannel();
+setInterval(checkYouTubeChannel, process.env.INTERVAL_MS);
+
+async function checkYouTubeChannel() {
+	const feed = await parser.parseURL(RSS_FEED);
+	const mostRecent = feed.items[0].link;
+	await db.put('mostRecent', mostRecent);
+	let lastSeen;
+	try {
+		lastSeen = await db.get('lastSeen');
+	} catch (err) {
+		if (err.notFound) {
+			console.log('created database');
+			lastSeen = mostRecent;
+			await db.put('lastSeen', mostRecent);
+		} else {
+			throw err;
+		}
+	}
+	let newVideos = 0;
+	for (const item of feed.items) {
+		if (item.link === lastSeen) {
+			break;
+		}
+		newVideos++;
+	}
+	const color = chooseColor(newVideos);
+	await blink(color);
+}
+
+function chooseColor(newVideos) {
+	switch (newVideos) {
+		case 0:
+			return '000000';
+		case 1:
+			return '0000ff';
+		case 2:
+			return '00ff00';
+		case 3:
+			return 'ffff00';
+		default:
+			return 'ff0000';
+	}
+}
+
+function blink(color) {
+	return new Promise(function (resolve, reject) {
+		childProcess.exec(`blink1-tool.exe --rgb ${color}`, (err, stdout, stderr) => {
+			if (err) {
+				reject(err);
+				return;
+			}
+			resolve();
+		});
+	});
+}
